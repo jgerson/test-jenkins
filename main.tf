7b4138f30968ec13461e97ace3b0b529d1756010
Jim Ray <ray.jim@gmail.com>
2019-06-09 14:41:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,40 +6,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "jray-demo-ec2" {
+resource "aws_instance" "jray-demo-jenkins" {
   ami           		 = "ami-b374d5a5"
   instance_type          = "t2.small"
-  vpc_security_group_ids = ["${aws_security_group.jray-demo-sg.id}"]
 
   tags {
-    Name = "TFE-VCS-example"
+    Name = "jray-demo-jenkins-run"
     owner = "jray@hashicorp.com"
     ttl = "2"
   }
-}
-
-resource "aws_security_group" "jray-demo-sg" {
-    name = "jray-demo-sg"
-    description = "jray 22&8080 inbound ANY outbound"
-   
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["172.16.0.0/16"]
-    }
-    
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["172.16.0.0/16"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 }
